9a7592bf16ffca77b3e5ffa954fdb4f8be394d92
eemmaa83 <emmacalvo83@gmail.com>
2026-01-10 15:04:11 +0100

    ajout nuit

---
+++ project_name/Nuit.h
@@ -0,0 +1,15 @@
+/*********************************************************************
+ * @file  NUIT.h
+ * @author Emma,Raksika,Pauline
+ * @brief Deroulement d'une nuit
+ *********************************************************************/
+
+#ifndef NUIT_H_
+#define NUIT_H_
+
+#include "Arduino.h"
+#include "string.h"
+#include "Joueur.h"
+
+
+class Nuit {}
