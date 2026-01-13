a1f4b35dc3aaa66da09fd478fa4e38e6547622f2
eemmaa83 <emmacalvo83@gmail.com>
2026-01-13 17:04:39 +0100

    ajout d'audio dans jour

--- project_name/Jour.cpp
+++ project_name/Jour.cpp
@@ -13,6 +13,7 @@ void Jour::lancerLeJour() {
     ecranRef.setRGB(255, 255, 0);
 
     ecranRef.afficherMessage("Recapitulatif", "des scores...");
+    sonRef.playSong(2);
     ecranRef.setRGB(255, 255, 255); 
     delay(2000);
 
@@ -24,20 +25,26 @@ void Jour::lancerLeJour() {
         ecranRef.afficherMessage(ligne1, ligne2);
         delay(3000); // 3 secondes pour lire par joueur
     }
+     
 
-
+    //peut-être afficher qui est mort
+    sonRef.playSong(3);
     ecranRef.afficherMessage("Vote du village", "Qui sauver ?"); 
-    delay(2000);
+    delay(10000);
     ecranRef.afficherMessage("Appuyez sur", "un joueur");
+    delay(10000);
+    sonRef.playSong(4);
 
     int elu = selectionnerJoueur();
 
     jeuRef.retirerGorgees(elu); 
     jeuRef.retirerGorgees(elu); 
     
-    ecranRef.afficherMessage("Le joueur " + String(elu + 1), "perd 2 gorgees");
+    sonRef.playSong(5);
+    ecranRef.afficherMessage("Le joueur " + String(elu), "perd 2 gorgees");
     delay(3000);
 
+    sonRef.playSong(2);
     ecranRef.afficherMessage("Recapitulatif", "des scores...");
     ecranRef.setRGB(255, 255, 255); 
     delay(2000);
@@ -45,12 +52,13 @@ void Jour::lancerLeJour() {
     for (int i = 0; i < 6; i++) {
         Joueur* p = jeuRef.getJoueur(i);
 
-        String ligne1 = "Joueur " + String(i + 1) + ;
+        String ligne1 = "Joueur " + String(i) + ;
         String ligne2 = String(p->getNbGorgees()) + " gorgees";
         ecranRef.afficherMessage(ligne1, ligne2);
         delay(3000); // 3 secondes pour lire par joueur
     }
 
+    //faire audio "retour à la nuit"
     ecranRef.afficherMessage("Fin du tour", "La nuit revient");
     delay(2000);
     ecranRef.setRGB(0, 0, 50); // Ecran bleu nuit un peu sombre
